f2da1b0ef1503de6cb654152c8bc96b838a3e1da
afatoga <afatoga@gmail.com>
2021-04-06 15:37:03 +0200

    static

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -6,14 +6,59 @@
         alt="Vuetify.js"
         class="mb-5"
       >
-      <blockquote class="blockquote">
+      <h1 v-html="title"></h1>
+      <div v-html="content">
+      </div>
+      <!-- <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
           </small>
         </footer>
-      </blockquote>
+      </blockquote> -->
     </v-col>
   </v-row>
 </template>
+
+<script>
+import axios from "axios"
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+
+  name: "Inspire",
+  // props: ["resourceName"],
+
+   data: () => ({
+    title: null,
+    content: null
+  }),
+
+  methods: {
+    prepareOutput: function(data) {
+      this.title = data[0].title.rendered
+      this.content = data[0].content.rendered
+    }
+  },
+
+    mounted() {
+    const options = {
+      url: `https://sw.tode.cz/wp-json/wp/v2/pages`,
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      // params: {
+      //   resourceName: this.resourceName,
+      //   limit: 10
+      // },
+    }
+
+    //.then(res => res.json())
+    axios(options).then((res) => this.prepareOutput(res.data))
+  },
+ 
+}
+</script>
